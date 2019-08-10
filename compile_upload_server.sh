2a1cc2c3168a8e2894d80762a5f5feb34b3846ef
Masterchef365 <duncan.freeman1@gmail.com>
2019-08-09 20:46:18 -0700

    Delete extra shell scripts

--- compile_upload_server.sh
+++
@@ -1,3 +0,0 @@
-host=team955@10.42.0.114
-cross build --target=aarch64-unknown-linux-gnu &&
-    scp target/aarch64-unknown-linux-gnu/debug/{server,test_client,aimcjog} $host:~/
--- testme.sh
+++
@@ -1,3 +0,0 @@
-host=nvidia@192.168.1.2
-#cross build --target=aarch64-unknown-linux-gnu &&
-    scp target/aarch64-unknown-linux-gnu/debug/{server,test_client,aimcjog} $host:~/
